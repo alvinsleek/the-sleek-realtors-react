afb4462f5c1d82baa2f6cc80680e0ec142497528
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-03-30 08:09:55 +0300

    feat:debuggiing

---
+++ the-sleek-realtors/src/components/login/Login.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from "react"
+function Login () {
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+
+  useEffect(() => {
+    document.getElementById("email").value = email
+    document.getElementById("password").value = password
+  },[email, password])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({email, password})
+    })
+    .then(() => window.location.reload())
+  }
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+
+      <button type="submit">Log In</button>
+    </form>
+
+
+
+   
+   
+  )
+}
+
+export default Login;
