c89cde38a14a40c52f7037b69934722d350bceb1
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-04-03 12:28:42 +0300

    debugging admin

--- the-sleek-realtors/src/components/admin/Admin.jsx
+++ the-sleek-realtors/src/components/admin/Admin.jsx
@@ -2,61 +2,96 @@ import React, { useState, useEffect } from "react";
 import Table  from "react-bootstrap/Table";
 
 const Admin = () =>{
-    const [tenants, SetTenants] = useState([]);
+    const [rentals, setRentals] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:4000/book").then((response) => {
-            SetTenants(response.data);
-        });
-    }, []);
+      const fetchRentals = async () => {
+        try {
+          const response = await fetch('api/v1/rentals');
+          const data = await response.json();
+          setRentals(data);
+        } catch (error) {
+          console.log(error);
+      }
+    };
+    fetchRentals();
+  }, []);
+
+  const columns = [{
+    Header: 'FirstName',
+    accessor:'firstName'
+  },
+  {
+    Header: 'LastName',
+    accessor:'lastName'
+  },
+  {
+    Header: 'Email',
+    accessor: 'email'
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone'
+  },
+  {
+    Header: 'Property',
+    accessor: 'property'
+  },
+  {
+    Header: 'Move-In Date',
+    accessor: 'moveInDate'
+  }
+]
 
     return(
-        <div>
-            <h2>Tenants List</h2>
+      <Table data={rentals} columns={columns}/>
+    );
+        // <div>
+        //     <h2>Tenants List</h2>
             
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Email</th>
-                  <th>Phone</th>
-                  <td>Property</td>
-                  <td>Move-In-Date</td>
+        //     <Table striped bordered hover>
+        //       <thead>
+        //         <tr>
+        //           <th>#</th>
+        //           <th>First Name</th>
+        //           <th>Last Name</th>
+        //           <th>Email</th>
+        //           <th>Phone</th>
+        //           <td>Property</td>
+        //           <td>Move-In-Date</td>
 
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>markotto@gmail.com</td>
-                  <td>+3345319158</td>
-                  <td>Red Carpet Real Estate</td>
-                  <td input itemType="date">03-04-2023</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@jacob@gmail.com</td>
-                  <td>+2574912712</td>
-                  <td>Fairmount Properties</td>
-                  <td>12-04-2023</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>Larry</td>
-                  <td>Aswan</td>
-                  <td>larry@gmail.com</td>
-                  <td>+2542832004</td>
-                  <td>Banyon Tree Realty</td>
-                  <td>30-05-2023</td>
-                </tr>
-              </tbody>
-            </Table>
+        //         </tr>
+        //       </thead>
+        //       <tbody>
+        //         <tr>
+        //           <td>1</td>
+        //           <td>Mark</td>
+        //           <td>Otto</td>
+        //           <td>markotto@gmail.com</td>
+        //           <td>+3345319158</td>
+        //           <td>Red Carpet Real Estate</td>
+        //           <td input itemType="date">03-04-2023</td>
+        //         </tr>
+        //         <tr>
+        //           <td>2</td>
+        //           <td>Jacob</td>
+        //           <td>Thornton</td>
+        //           <td>@jacob@gmail.com</td>
+        //           <td>+2574912712</td>
+        //           <td>Fairmount Properties</td>
+        //           <td>12-04-2023</td>
+        //         </tr>
+        //         <tr>
+        //           <td>3</td>
+        //           <td>Larry</td>
+        //           <td>Aswan</td>
+        //           <td>larry@gmail.com</td>
+        //           <td>+2542832004</td>
+        //           <td>Banyon Tree Realty</td>
+        //           <td>30-05-2023</td>
+        //         </tr>
+        //       </tbody>
+        //     </Table>
   
             {/* <table >
                 <thead>
@@ -83,8 +118,8 @@ const Admin = () =>{
                     </tbody>
                 
                 </table> */}
-        </div>
-    );
+        // </div>
+    
 };
 
 export default Admin;
