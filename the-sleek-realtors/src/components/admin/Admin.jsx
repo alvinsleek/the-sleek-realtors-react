d87d8b1380dafcf66046b7342a517fb10f241acb
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-04-03 15:58:25 +0300

    modified admin page

--- the-sleek-realtors/src/components/admin/Admin.jsx
+++ the-sleek-realtors/src/components/admin/Admin.jsx
@@ -2,96 +2,100 @@ import React, { useState, useEffect } from "react";
 import Table  from "react-bootstrap/Table";
 
 const Admin = () =>{
-    const [rentals, setRentals] = useState([]);
+    const [tenants, setTenants] = useState([]);
 
     useEffect(() => {
-      const fetchRentals = async () => {
-        try {
-          const response = await fetch('api/v1/rentals');
-          const data = await response.json();
-          setRentals(data);
-        } catch (error) {
-          console.log(error);
-      }
-    };
-    fetchRentals();
-  }, []);
+      fetch("http://localhost:4000/rentals").then((response) => {
+        setTenants(response.data);
+      });
+    }, []);
+  //     const fetchRentals = async () => {
+  //       try {
+  //         const response = await fetch('api/v1/rentals');
+  //         const data = await response.json();
+  //         setRentals(data);
+  //       } catch (error) {
+  //         console.log(error);
+  //     }
+  //   };
+  //   fetchRentals();
+  // }, []);
 
-  const columns = [{
-    Header: 'FirstName',
-    accessor:'firstName'
-  },
-  {
-    Header: 'LastName',
-    accessor:'lastName'
-  },
-  {
-    Header: 'Email',
-    accessor: 'email'
-  },
-  {
-    Header: 'Phone',
-    accessor: 'phone'
-  },
-  {
-    Header: 'Property',
-    accessor: 'property'
-  },
-  {
-    Header: 'Move-In Date',
-    accessor: 'moveInDate'
-  }
-]
+  // const columns = [{
+  //   Header: 'FirstName',
+  //   accessor:'firstName'
+  // },
+  // {
+  //   Header: 'LastName',
+  //   accessor:'lastName'
+  // },
+  // {
+  //   Header: 'Email',
+  //   accessor: 'email'
+  // },
+  // {
+  //   Header: 'Phone',
+  //   accessor: 'phone'
+  // },
+  // {
+  //   Header: 'Property',
+  //   accessor: 'property'
+  // },
+  // {
+  //   Header: 'Move-In Date',
+  //   accessor: 'moveInDate'
+  // }
+// ]
 
     return(
-      <Table data={rentals} columns={columns}/>
-    );
-        // <div>
-        //     <h2>Tenants List</h2>
+      
+    
+        <div>
+            <h2>Tenants List</h2>
             
-        //     <Table striped bordered hover>
-        //       <thead>
-        //         <tr>
-        //           <th>#</th>
-        //           <th>First Name</th>
-        //           <th>Last Name</th>
-        //           <th>Email</th>
-        //           <th>Phone</th>
-        //           <td>Property</td>
-        //           <td>Move-In-Date</td>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <td>Property</td>
+                  <td>Move-In-Date</td>
 
-        //         </tr>
-        //       </thead>
-        //       <tbody>
-        //         <tr>
-        //           <td>1</td>
-        //           <td>Mark</td>
-        //           <td>Otto</td>
-        //           <td>markotto@gmail.com</td>
-        //           <td>+3345319158</td>
-        //           <td>Red Carpet Real Estate</td>
-        //           <td input itemType="date">03-04-2023</td>
-        //         </tr>
-        //         <tr>
-        //           <td>2</td>
-        //           <td>Jacob</td>
-        //           <td>Thornton</td>
-        //           <td>@jacob@gmail.com</td>
-        //           <td>+2574912712</td>
-        //           <td>Fairmount Properties</td>
-        //           <td>12-04-2023</td>
-        //         </tr>
-        //         <tr>
-        //           <td>3</td>
-        //           <td>Larry</td>
-        //           <td>Aswan</td>
-        //           <td>larry@gmail.com</td>
-        //           <td>+2542832004</td>
-        //           <td>Banyon Tree Realty</td>
-        //           <td>30-05-2023</td>
-        //         </tr>
-        //       </tbody>
-        //     </Table>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>markotto@gmail.com</td>
+                  <td>+3345319158</td>
+                  <td>Red Carpet Real Estate</td>
+                  <td input itemType="date">03-04-2023</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@jacob@gmail.com</td>
+                  <td>+2574912712</td>
+                  <td>Fairmount Properties</td>
+                  <td>12-04-2023</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Larry</td>
+                  <td>Aswan</td>
+                  <td>larry@gmail.com</td>
+                  <td>+2542832004</td>
+                  <td>Banyon Tree Realty</td>
+                  <td>30-05-2023</td>
+                </tr>
+              </tbody>
+            </Table>
   
             {/* <table >
                 <thead>
@@ -105,7 +109,7 @@ const Admin = () =>{
                 </tr>
                 </thead>
                 <tbody>
-                    {tenants.map((tenant, index) => (
+                    {rentals.map((tenant, index) => (
                         <tr key={tenant.id}>
                             <th scope="row">{index + 1}</th>
                             <th>{tenant.first_name}</th>
@@ -118,7 +122,12 @@ const Admin = () =>{
                     </tbody>
                 
                 </table> */}
-        // </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+         </div>
+    )
     
 };
 
