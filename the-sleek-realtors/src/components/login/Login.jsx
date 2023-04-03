f377df5321774b0125853435c63ba2c66b2559da
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-04-03 18:13:07 +0300

    debugging

--- the-sleek-realtors/src/components/login/Login.jsx
+++ the-sleek-realtors/src/components/login/Login.jsx
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from "react"
 function Login () {
+  
+  // state variables are initialized to hold email and password
   const[email, setEmail] = useState('')
   const[password, setPassword] = useState('')
 
+  //useEffect to set value in the input fields
   useEffect(() => {
     document.getElementById("email").value = email
     document.getElementById("password").value = password
   },[email, password])
 
+  // handleSubmit to handle on the form submission
   const handleSubmit = (e) => {
     e.preventDefault()
-
+// A POST request is sent to 'login' route containing email and password
     fetch('/login', {
       method: 'POST',
       headers: {
@@ -18,6 +22,7 @@ function Login () {
       },
       body: JSON.stringify({email, password})
     })
+    // page is reloaded after request
     .then(() => window.location.reload())
   }
   return (
--- the-sleek-realtors/src/components/signup/Signup.jsx
+++ the-sleek-realtors/src/components/signup/Signup.jsx
@@ -6,26 +6,29 @@ import "../home/recent/recent.css"
 import img from "../images/about.jpg"
 import axios from "axios";
 
-
+//define the signup component
 const Signup = () => {
-  const history = useHistory();
-  
+  const history = useHistory(); // the useHistory object is gotten from the react-router-dom module
+  // state variables used are initialized using UseState
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+// handlesubmit function which is called upon submitting the form
   const handleSubmit = async (event) => {
     event.preventDefault();
     const user = {name: name, email, password};
     const response = await axios.post("http://localhost:3000/api/v1/users", { user });
     console.log(response);
-     window.location.href = "/login";
+     window.location.href = "/login";// user is redirected to the login page
  
+
+     //browser history gets updated and pushes user's name to the state object
     history.push({
       pathname:"/",
       state: {username: name },
     })
   };
+  //render
   return (
     <form onSubmit={handleSubmit} action="/signup" method="POST">
       <label htmlFor="name">Name:</label>
